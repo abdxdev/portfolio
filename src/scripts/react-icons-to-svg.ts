b8d3e02290b9e49092de3b4b15e2db053ce85029
Abdul Rahman <abdulrahman.abd.dev@gmail.com>
2026-04-04 18:03:19 +0500

    feat: update SVG icons for email, portfolio, and resume; add new script for generating icons from react-icons

--- src/scripts/react-icons-to-svg.ts
+++ src/scripts/react-icons-to-svg.ts
@@ -1,9 +1,9 @@
 import { createElement } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
-import { FiLink } from "react-icons/fi";
-
+import fs from "fs";
+import { IoGlobe  } from "react-icons/io5";
 function icon_string(Icon: React.ComponentType<any>): string {
   return renderToStaticMarkup(createElement(Icon));
 }
 
-console.log(icon_string(FiLink));
+fs.writeFileSync("public/assets/icons/portfolio.svg", icon_string(IoGlobe ));
